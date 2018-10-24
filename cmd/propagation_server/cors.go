453521cdd2a06d9761be8e8405a494cab7aac64d
Ivan Danyliuk <ivan.daniluk@gmail.com>
2018-10-24 20:01:47 +0200

    Kinda improve CORS headers

--- cmd/propagation_server/cors.go
+++ cmd/propagation_server/cors.go
@@ -7,7 +7,7 @@ func allowCORS(fn func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc
 		if origin := r.Header.Get("Access-Control-Allow-Origin"); origin == "" {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 		}
-		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		if origin := r.Header.Get("Origin"); origin != "" {
 			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
 				// set preflight options
